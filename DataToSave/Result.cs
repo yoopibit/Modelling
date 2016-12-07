06a6f83035d4162445201b618eac673439d093f8
yoopibit <yavorskyi.yurii@gmail.com>
2016-12-08 01:51:05 +0200

    add result class

---
+++ DataToSave/Result.cs
@@ -0,0 +1,34 @@
+﻿using DataToSave.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataToSave
+{
+    [DataContract]
+    public class Result
+    {
+        [DataMember]
+        public IEnumerable<String> GamerNames { get; set; }
+        public IEnumerable<int> CurrentResult { get; set; }
+
+        public static string DataPath = "result.dat";
+
+        public static Result Load()
+        {
+            if (File.Exists(DataPath))
+            {
+                return (Result)DataSerializer.DeserializeItem(DataPath, typeof(Result));
+            }
+            return new Result();
+        }
+        public void Save()
+        {
+            DataSerializer.SerializeData(DataPath, typeof(Result), this);
+        }
+    }
+}
---
+++ DataToSave/Serialization/DataSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataToSave.Serialization
+{
+    [DataContract]
+    public class DataSerializer
+    {
+        public static void SerializeData(string fileName, Type type, object data)
+        {
+            var formatter = new DataContractSerializer(type);
+            var s = new FileStream(fileName, FileMode.Create);
+            formatter.WriteObject(s, data);
+            s.Close();
+        }
+
+        public static object DeserializeItem(string fileName, Type type)
+        {
+            var s = new FileStream(fileName, FileMode.Open);
+            var formatter = new DataContractSerializer(type);
+            return formatter.ReadObject(s);
+        }
+    }
+}
